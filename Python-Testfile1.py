83b059262afa864361659b9dca7955e129f0e31d
AzizaAdam <drsazizaadam@gmail.com>
2022-02-08 10:09:31 +0100

    commit

--- Python-Testfile1.py
+++ Python-Testfile1.py
@@ -1,1 +1,47 @@
 print("Hello, world")
+x = 4       # x is of type int
+x = "Sally" # x is now of type str
+print(x)
+
+x = 5
+y = "John"
+print(type(x))
+print(type(y))
+
+x, y, z = "Orange", "Banana", "Cherry"
+print(x)
+print(y)
+print(z)
+
+x = y = z = "Orange"
+print(x)
+print(y)
+print(z)
+
+fruits = ["apple", "banana", "cherry"]
+x, y, z = fruits
+print(x)
+print(y)
+print(z)
+
+var1 = x = 5
+var2 = y = 2
+print(x)
+print(y)
+
+var3 = z = x + y
+print(z)
+
+Greeting = "Hello"
+name = "Aziza!"
+print( Greeting, name)
+
+var1 = 7
+print("value 1:", var1)
+
+var1 = 99
+print("value 2:", var1)
+
+var1 = "You got the point"
+print("value 3:", var1)
+
